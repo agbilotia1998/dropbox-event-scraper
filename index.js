c96ed19dd69a65820a827e0a36a94e3bf7ac36dd
agbilotia1998 <agb.ayushgupta@gmail.com>
2021-01-20 23:53:43 +0530

    Adds functionality for following the event link and store dataLink

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 
 const options = require("./options.json")
 
-const PAGE_SIZE = 250
+const PAGE_SIZE = 25
 const CSV_OUTPUT_PATH = './output.csv'
-const START_DATE = 'November 10, 2020 00:00:00 GMT+00:00'
+const START_DATE = 'January 18, 2021 00:00:00 GMT+00:00'
 const START_EPOCH_TIME = Math.round(new Date(START_DATE).getTime() / 1000)
 
-const END_DATE = 'January 16, 2021 00:00:00 GMT+00:00'
+const END_DATE = 'January 22, 2021 00:00:00 GMT+00:00'
 const END_EPOCH_TIME = Math.round(new Date(END_DATE).getTime() / 1000)
 let epochTime = END_EPOCH_TIME
 
@@ -28,24 +28,59 @@ let getData = async () => {
     console.log(date)
 
     let data = await fetch("https://www.dropbox.com/events/ajax", options);
-
     return data.json();
 }
 
-let parseAndSave = async(data) => {
-    return data.then(data => {
-        data.events = data.events.filter((eventDetail) => {
-            return eventDetail['is_dup'] === false
-        })
-        const fields = ['name', 'timestamp', 'ago', 'event_blurb', {
-            label: 'blurb',
-            value: (item) => {
-                return htmlToText(item['blurb'])
-            }
-        }];
-        const opts = {fields};
+let getEventText = async (url) => {
+    let optionsGetRequest = options
 
+    optionsGetRequest.method = 'GET'
+    delete optionsGetRequest['body']
+
+    let data = await fetch(url, optionsGetRequest)
+    let text = await data.text()
+    let regex = /(.*?)InitReact(.*?)event_details(.*?)"entries":(.*?), "changesetId"/g
+    let match = regex.exec(text)
+    if(match != null) {
+        let jsonResponse = JSON.parse(match[4])
+
+        return new Promise(resolve => {
+            resolve(jsonResponse[0].fileUrl.slice(2))
+        })
+    }
+}
+
+let parseAndSave = async(data) => {
+    return data.then(async data => {
         try {
+            data.events = data.events.filter((eventDetail) => {
+                return eventDetail['is_dup'] === false
+            })
+
+            const promises = data.events.map(async (eventDetail) => {
+                let regex = /(.*?)href='(.*?)'/
+
+                if (regex.exec(eventDetail['event_blurb']) != null) {
+                    let dataLink = await getEventText("https://www.dropbox.com" + regex.exec(eventDetail['event_blurb'])[2])
+                    console.log(dataLink)
+
+                    eventDetail['dataLink'] = dataLink
+
+                    return new Promise(resolve => {
+                        resolve(dataLink)
+                    })
+                }
+            })
+            await Promise.all(promises)
+
+            const fields = ['name', 'timestamp', 'ago', 'event_blurb', {
+                label: 'blurb',
+                value: (item) => {
+                    return htmlToText(item['blurb'])
+                }
+            }, 'dataLink'];
+            const opts = {fields};
+
             const parser = new json2csv.Parser(opts);
             const csvData = parser.parse(data.events);
             let totalEvents = data.events.length
@@ -81,7 +116,7 @@ let main = async() => {
                 break
             }
 
-            await sleep(20000)
+            await sleep(5000)
         } catch (err) {
             console.log(err.message)
         }
